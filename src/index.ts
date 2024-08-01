ad371c3ca3f8a963ae8030e39862f7a4c29d4e66
vinimostaco <vini.mostaco@hotmail.com>
2024-07-31 21:40:02 -0300

    initial commit with method to create and list user

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Vinicius",
+      email: "vinicius.mostaco@alest.com.br",
+    },
+  });
+
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
